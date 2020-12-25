cb9501b6c955b4c70e0912cc2dc43b9c5b410156
Devy Febriyanti H <45996829+dvfebriyanti18@users.noreply.github.com>
2020-12-26 00:17:39 +0700

    Add files via upload
    Tampilan

---
+++ index.php
@@ -0,0 +1,338 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>LE PESTO</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">LE PESTO</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About Us</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">LE PESTO</h1>
+            <p class="lead">Temukan Restaurant Terbaik di Kota Bandung</p>
+        </div>
+    </div>
+
+    <!-- location -->
+
+    <section class="location " id="location">
+        <div class="container pt-5 mb-5 pb-5 container-lokasi">
+            <div class="row pt-4 mb-4">
+                <div class="col text-center">
+                    <h2 class="pt-3">Daerah Populer Di Kota Bandung</h2>
+                </div>
+            </div>
+            <div class="container">
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Riau</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Dago</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Dago Pakar</p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Lengkong</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Sukajadi</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Buahbatu</p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Sumur Bandung</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Lembang</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Ciumbuleuit</p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Sersan Bajuri</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Bandung Wetan</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Cihampelas</p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Braga</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Pasteur</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Cikutra</p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Cicendo</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Andir</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Pasir Kaliki</p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">GegerKalong</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Surapati</p>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="card-text">Pungkur</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- About -->
+        <section class="about pt-2" id="about">
+            <div class="container pt-5 mb-5">
+                <div class="row mb-4">
+                    <div class="col text-center">
+                        <h2>About</h2>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-5 pb-5"><br>
+                        <img src="img/makanan/3.jpg" alt="" style="width: 400px;">
+                    </div>
+                    <div class="col-md-5 pb-5" style="text-align: justify;">
+                        <p>Le Pesto merupakan website yang menyediakan informasi sebuah restaurant yang ada di Kota Bandung. Le Pesto hadir untuk mempermudah kita untuk menjelajahi Restaurant terbaik berdasarkan Tren dan rekomendasi dari customernya. Dan website ini juga sangat membantu pengguna untuk mencari informasi terkait restaurant-restaurant yang ada di sekitar Kota Bandung, beserta informasi nama tempat, lokasi, dan sebagainya. <br> <br>
+                            Dengan adanya website ini tentunya sangat membantu pengguna untuk menemukan restaurant yang sesuai dengan kriteria yang diinginkan. Jelajahi restaurant yang anda inginkan menggunakan website Le Pesto. Kami Pastikan anda akan terbantu dengan informasi yang kami hadirkan dan kami sediakan.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Footer -->
+        <footer class="bg-footer text-black text-center text-lg-start pt-2">
+            <!-- Grid container -->
+            <div class="container p-4 pt-5">
+                <!--Grid row-->
+                <div class="row">
+                    <!--Grid column-->
+                    <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+                        <h5 class="text-uppercase" style="font-size: 35px;">Le Pesto</h5>
+
+                        <p>
+                            Universitas Pasundan
+                        </p>
+                        <p class="pt-0">
+                            Jalan Dr. Setiabudi No 193, Bandung
+                        </p>
+                        <p>
+                            <i class="fab fa-instagram"></i>
+
+
+
+                        </p>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase">Links</h5>
+
+                        <ul class="list-unstyled mb-0 text-black">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                        <h5 class="text-uppercase mb-0">Links</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <a href="#!" class="text-white">Link 1</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 2</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 3</a>
+                            </li>
+                            <li>
+                                <a href="#!" class="text-white">Link 4</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+                </div>
+                <!--Grid row-->
+            </div>
+            <!-- Grid container -->
+            <hr class="hrr">
+            <!-- Copyright -->
+            <div class="text-center p-3" style=" background-color: rgb(247, 247, 247)">
+                © 2020 Copyright:
+                <a class="text-black" style="text-decoration:none; color:black" href="">Lepesto</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+        <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
